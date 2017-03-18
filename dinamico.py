ceb4ca5b408e8b240c5e7c7c0b926ca5e61c8437
Goma <gbrmachado@gmail.com>
2017-03-18 20:16:10 -0300

    Adiciona algoritmo dinamico

--- dinamico.py
+++ dinamico.py
@@ -9,3 +9,29 @@ from classes.leitura import Leitura
 
 NOME_ARQUIVO = sys.argv[1] # Obtem o nome do arquivo a ser lido
 LISTA_ATIVIDADES = Leitura(NOME_ARQUIVO).get_atividades()
+
+LISTA_ATIVIDADES.sort(key = lambda x: x.get_fim())  #ordena lista de atividades
+
+NUM_JOBS = len(LISTA_ATIVIDADES)
+
+T = [1]*NUM_JOBS #Define uma lista contendo os pesos dos trabalhos. Todos iguais a um.
+
+for j in range(1, NUM_JOBS): #Gera tabela contendo o numero de atividades maximo ate entao
+    for i in xrange(j):
+        if LISTA_ATIVIDADES[i].get_fim() <= LISTA_ATIVIDADES[j].get_inicio():
+            T[j] = max(T[j], T[i] + 1)
+
+i = NUM_JOBS - 1
+while i >= 0:
+    #calcula o mais proximo
+    j = i-1
+    while LISTA_ATIVIDADES[j].get_fim() >= LISTA_ATIVIDADES[i].get_inicio() and j > 0:
+        j = j - 1
+
+    if 1 + T[j] > T[i-1]:
+        LISTA_ATIVIDADES[i].print_atividade()
+        i = j
+    else:
+        i = i-1
+
+print "numero maximo de Tarefas: "+ str( max(T))
